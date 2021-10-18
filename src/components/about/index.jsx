f0445f470e335eae216a5e173a88f69effc285d9
hishamtk <tkhisham007@gmail.com>
2021-10-18 20:53:10 +0530

    add About me page

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -25,10 +25,10 @@ const About = () => {
               <SubTitle> Front End Developer</SubTitle>
             </div> 
             <div>
-              <Title> Hello, I’m AbdAli </Title>
-              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">North Africa</b> living in Casablanca. </Text>
+              <Title> Hello, I’m Hisham </Title>
+              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">India</b> living in Calicut. </Text>
               <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
-              <Text> behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
+              <Text> The Hacking School Graduate, love to work with great people to make a difference.
               </Text>
               <ResumeButton href="resume.pdf" target="_blank"> Download resume </ResumeButton>
             </div>
